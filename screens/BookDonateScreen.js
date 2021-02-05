3722f9d7510f96f75b61c8e306d366a0c51e21c3
Manasa <Manasa9955j@gmail.com>
2021-02-05 20:55:14 +0530

    c81+

--- screens/BookDonateScreen.js
+++ screens/BookDonateScreen.js
@@ -1,15 +1,70 @@
 import * as React from "react";
-import {View,TextInput,TouchableOpacity,Text,StyleSheet} from "react-native";
+import {View,TextInput,TouchableOpacity,Text,StyleSheet,FlatList} from "react-native";
+import db from "../config";
+import firebase from "firebase";
+import {ListItem} from "react-native-elements";
+import MyHeader from "../components/MyHeader";
 
 export default class BookDonateScreen extends React.Component{
-   
-        
-    
+   constructor(){
+       super();
+       this.state={
+           userId:firebase.auth().currentUser.email,
+           requestedBookList:[]
+       }
+       this.requestRef=null;
+
+   } 
+   getBookList=()=>{
+      this.requestRef= db.collection("BookRequest").onSnapshot((snapShot)=>{
+          var requestedBookList=snapShot.docs.map(doc=>doc.data())
+          this.setState({
+              requestedBookList:requestedBookList
+          })
+      })
+      
+   }  
+    componentDidMount(){
+        this.getBookList();
+    }
+    componentWillUnmount(){
+        this.requestRef();
+    }
+    keyExtractor=(item,index)=>{
+        index.toString();
+    }
+    renderItem=({item,i})=>{
+       return(
+           <ListItem 
+           key={i}
+           title={item.bookName}
+           subtitle={item.reasonToRequest}
+           titleStyle={{color:"blue",fontWeight:"bold"}}
+           rightElement={
+               <TouchableOpacity><Text>Donate</Text></TouchableOpacity>
+           }
+           />
+       )
+    }
     render(){
         return(
             <View>
-                <Text>Book donate Screen</Text>
-               
+                <MyHeader/>
+               <View>{
+                this.state.requestedBookList.length===0?(
+                    <View>
+                        <Text>Loading.......</Text>
+                    </View>
+                ):(
+                    <FlatList 
+                    keyExtractor={this.keyExtractor}
+                    data={this.state.requestedBookList}
+                    renderItem={this.renderItem}
+                    />
+                )
+
+                    }
+                </View>
                
 
                
---
+++ screens/receiverDetailscreen.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import {View,Text} from "react-native";
+
+export default class ReceiverDetailScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Tex>ReceiverDetailScreen</Tex>
+            </View>
+        )
+    }
+}
